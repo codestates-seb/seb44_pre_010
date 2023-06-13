191c8910847be7f942c178b7485fd87dea300888
Dayoung <dbsek21@naver.com>
2023-06-13 18:35:12 +0900

    chore: 배포 스크립트 경로 변경

--- server/deploy/deploy.sh
+++ server/deploy/deploy.sh
@@ -4,27 +4,27 @@
 BUILD_JAR=$(ls /var/lib/jenkins/workspace/pre-project/server/build/libs/sof-0.0.1-SNAPSHOT.jar)
 JAR_NAME=$(basename $BUILD_JAR)
 
-echo "> 현재 시간: $(date)" >> ~/log/deploy.log
+echo "> 현재 시간: $(date)" >> /home/ec2-user/log/deploy.log
 
-echo "> build 파일명: $JAR_NAME" >> ~/log/deploy.log
+echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/log/deploy.log
 
-echo "> build 파일 복사" >> ~/log/deploy.log
-DEPLOY_PATH=~/build/
+echo "> build 파일 복사" >> /home/ec2-user/log/deploy.log
+DEPLOY_PATH=/home/ec2-user/build/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> ~/log/deploy.log
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/log/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> ~/log/deploy.log
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >>  /home/ec2-user/log/deploy.log
 else
-  echo "> kill -9 $CURRENT_PID" >> ~/log/deploy.log
+  echo "> kill -9 $CURRENT_PID" >> /home/ec2-user/log/deploy.log
   sudo kill -9 $CURRENT_PID
   sleep 5
 fi
 
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> ~/log/deploy.log
-sudo java -jar $DEPLOY_JAR >> ~/log/deploy.log 2>~/log/deploy_err.log &
+echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/log/deploy.log
+sudo java -jar $DEPLOY_JAR >> /home/ec2-user/log/deploy.log 2>/home/ec2-user/log/deploy_err.log &
