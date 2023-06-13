8dfedaff59a3b2598aa2846e31e4e57c2fac4663
Dayoung <dbsek21@naver.com>
2023-06-13 17:17:26 +0900

    chore: 배포 스크립트 관리자 권한 부여

--- server/deploy/deploy.sh
+++ server/deploy/deploy.sh
@@ -3,27 +3,27 @@
 BUILD_JAR=$(ls /var/lib/jenkins/workspace/pre-project/server/build/libs/sof-0.0.1-SNAPSHOT.jar)
 JAR_NAME=$(basename $BUILD_JAR)
 
-echo "> 현재 시간: $(date)" >> log/deploy.log
+sudo echo "> 현재 시간: $(date)" >> log/deploy.log
 
-echo "> build 파일명: $JAR_NAME" >> log/deploy.log
+sudo echo "> build 파일명: $JAR_NAME" >> log/deploy.log
 
-echo "> build 파일 복사" >> log/deploy.log
+sudo echo "> build 파일 복사" >> log/deploy.log
 DEPLOY_PATH=build/
-cp $BUILD_JAR $DEPLOY_PATH
+sudo cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> log/deploy.log
+sudo echo "> 현재 실행중인 애플리케이션 pid 확인" >> log/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> log/deploy.log
+  sudo echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> log/deploy.log
 else
-  echo "> kill -9 $CURRENT_PID" >> log/deploy.log
+  sudo echo "> kill -9 $CURRENT_PID" >> log/deploy.log
   sudo kill -9 $CURRENT_PID
-  sleep 5
+  sudo sleep 5
 fi
 
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> log/deploy.log
-sudo nohup java -jar $DEPLOY_JAR >> log/deploy.log 2>log/deploy_err.log &
+sudo echo "> DEPLOY_JAR 배포"    >> log/deploy.log
+sudo java -jar $DEPLOY_JAR >> log/deploy.log 2>log/deploy_err.log &
