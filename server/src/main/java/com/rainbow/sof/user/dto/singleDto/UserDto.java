3a027b4c7baddc46b0ad5623b1b1c21f4fd02bfe
younghoshin <sim6671@naver.com>
2023-06-16 10:13:15 +0900

    refactor: 비밀번호 유효성 검증 추가

--- server/src/main/java/com/rainbow/sof/user/dto/singleDto/UserDto.java
+++ server/src/main/java/com/rainbow/sof/user/dto/singleDto/UserDto.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
@@ -18,7 +19,9 @@ public class UserDto {
         private String name;
 
         @NotBlank
-        @Size(min = 8)
+        @Size(min = 8,message = "비밀번호는 최수 8자리 이상이어야합니다.")
+        @Pattern(regexp="^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+        message = "비밀번호는 하나이상의 숫자와 문자가 조합 되어야합니다.")
         private String password;
 
         @NotBlank
