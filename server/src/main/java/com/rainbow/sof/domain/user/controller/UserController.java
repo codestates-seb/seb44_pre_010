65b0054b376f096f8042290e4df538db1410a49b
younghoshin <sim6671@naver.com>
2023-06-20 17:27:06 +0900

    refactor: 커스텀 어노테이션 활용 및 기능 수정 - 커스텀 어노테이션으로 유저정보 활용 - 유저 삭제시 자격확인 기능 수정

--- server/src/main/java/com/rainbow/sof/domain/user/controller/UserController.java
+++ server/src/main/java/com/rainbow/sof/domain/user/controller/UserController.java
@@ -5,6 +5,7 @@ import com.rainbow.sof.domain.user.dto.singleDto.UserDto;
 import com.rainbow.sof.domain.user.entity.User;
 import com.rainbow.sof.domain.user.mapper.UserMapper;
 import com.rainbow.sof.domain.user.service.UserService;
+import com.rainbow.sof.global.common.AuthenticationName;
 import com.rainbow.sof.global.utils.UriCreator;
 import com.rainbow.sof.domain.user.dto.DataDto.UserDataResponse;
 import com.rainbow.sof.domain.user.dto.UserToJoinDto.MyPageResponseDto;
@@ -14,10 +15,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 import java.net.URI;
 import java.security.Principal;
+
+import static com.rainbow.sof.domain.user.util.CustomEnumUri.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -27,23 +31,23 @@ public class UserController {
 
     private final UserMapper mapper;
     private final UserService service;
-    private final static String USER_DEFAULT_URL = "/api/v1/users";
-    private final static String DELETE_ACTION_URL = "/signup";
 
     @PostMapping("/signup")
     public ResponseEntity<?> postSignup(@Valid @RequestBody UserDto.SignUpPost signUpPost){
         User user = mapper.userSignupPostToUser(signUpPost);
         User createUser=service.createUser(user);
-        URI location = UriCreator.createUri(USER_DEFAULT_URL, createUser.getUserId());
+        URI location = UriCreator.createUri(USER_DEFAULT_URL.getUri(), createUser.getUserId());
         return ResponseEntity.created(location).build();
     }
 
 
 //    @PostMapping("/login")
-//    public ResponseEntity<?> postLogin(@Valid @RequestBody UserDto.CreationLoginDto loginPost){
-//        User findUser = service.findByUserFromEmail(loginPost.getUsername());
-//        URI location = UriCreator.createUri("",1);
-//        return ResponseEntity.ok().location(location).build();
+//    public ResponseEntity<?> postLogin(@AuthenticationName String email
+//            , HttpServletResponse response){
+//        User user = service.getUserFromEmail(email);
+//        URI location = UriCreator.createUri(USER_DEFAULT_URL,user.getUserId());
+//        response.addHeader("Location", String.valueOf(location));
+//        return ResponseEntity.ok().body(user.getUserId());
 //    }
 
 //    @PostMapping("/logout")
@@ -54,9 +58,9 @@ public class UserController {
 //    }
 
     @GetMapping("/users/{user-id}")
-    public ResponseEntity<?> getUserData(Principal principal,
+    public ResponseEntity<?> getUserData(@AuthenticationName String email,
                                          @Valid @PathVariable("user-id") @Positive long id){
-        User user = service.checkToFindByUserFromEmail(principal.getName(),id);
+        User user = service.checkToFindByUserFromEmail(email,id);
         MyPageResponseDto myPageDto= mapper.userToMyPageDto(user);
         UserDataResponse response=UserDataResponse.builder().data(myPageDto).build();
         return new ResponseEntity<>(response, HttpStatus.OK);
@@ -65,7 +69,7 @@ public class UserController {
 //    @Valid @PathVariable("user-id") @Positive long id
 
     @PatchMapping("/users/{user-id}")
-    public ResponseEntity<?> patchUser(Principal principal,
+    public ResponseEntity<?> patchUser(@AuthenticationName String email,
                                          @Valid @PathVariable("user-id") @Positive long id,
                                        @RequestBody UserDto.Patch patch){
 
@@ -73,9 +77,11 @@ public class UserController {
     }
 
     @DeleteMapping("/users/{user-id}")
-    public ResponseEntity<?> deleteUser(@Valid @PathVariable("user-id") @Positive long id){
-        service.deleteUser(id);
-        URI location = UriCreator.createUri(DELETE_ACTION_URL);
+    public ResponseEntity<?> deleteUser(@AuthenticationName String email,
+            @Valid @PathVariable("user-id") @Positive long id){
+        User user = service.checkToFindByUserFromEmail(email,id);
+        service.deleteUser(user.getUserId());
+        URI location = UriCreator.createUri(DELETE_ACTION_URL.getUri());
         return ResponseEntity.noContent().location(location).build();
     }
 
