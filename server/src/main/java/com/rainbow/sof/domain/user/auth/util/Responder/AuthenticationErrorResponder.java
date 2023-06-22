c41c77d21f1d0b98d77b7e0cb0ed3c20932c37f7
younghoshin <sim6671@naver.com>
2023-06-22 10:00:32 +0900

    refactor: 메서드 추출 - 중복 코드 제거

--- server/src/main/java/com/rainbow/sof/domain/user/auth/util/Responder/AuthenticationErrorResponder.java
+++ server/src/main/java/com/rainbow/sof/domain/user/auth/util/Responder/AuthenticationErrorResponder.java
@@ -9,9 +9,18 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class AuthenticationErrorResponder {
-    public static void sendErrorResponse(HttpServletResponse response, HttpStatus status) throws IOException, IOException {
-        Gson gson = new Gson();
+    public static void sendErrorResponse(HttpServletResponse response, HttpStatus status,String message) throws IOException {
+        ErrorResponse errorResponse = ErrorResponse.of(status,message);
+        setResponse(response, status,errorResponse);
+    }
+
+    public static void sendErrorResponse(HttpServletResponse response, HttpStatus status) throws  IOException {
         ErrorResponse errorResponse = ErrorResponse.of(status);
+        setResponse(response, status,errorResponse);
+    }
+
+    private static void setResponse(HttpServletResponse response, HttpStatus status, ErrorResponse errorResponse) throws IOException {
+        Gson gson = new Gson();
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(status.value());
         response.getWriter().write(gson.toJson(errorResponse, ErrorResponse.class));
