80984aba89e335c2facf485a0753fd773767b0e7
Dayoung <dbsek21@naver.com>
2023-06-22 20:36:28 +0900

    fix: Cors 오류 수정 - localhost:3000 추가 - 테스트 서버 uri추가

--- server/src/main/java/com/rainbow/sof/global/config/WebConfig.java
+++ server/src/main/java/com/rainbow/sof/global/config/WebConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -15,6 +17,17 @@ import java.util.List;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private final AuthenticationArgumentResolver authenticationArgumentResolver;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/api/v1/**")
+                .allowedOrigins("http://localhost:3000","http://ec2-52-78-15-107.ap-northeast-2.compute.amazonaws.com")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedHeaders("Authorization","X-AUTH-TOKEN")
+                .allowCredentials(false)
+                .maxAge(3000);
+    }
+
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers){
