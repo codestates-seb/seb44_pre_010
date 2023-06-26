8f931359863ad7bf9bdff72d019795ca3a98dcf8
Dayoung <dbsek21@naver.com>
2023-06-26 21:02:51 +0900

    refactor: 모든 메소드 허용

--- server/src/main/java/com/rainbow/sof/domain/user/config/SecurityConfiguration.java
+++ server/src/main/java/com/rainbow/sof/domain/user/config/SecurityConfiguration.java
@@ -94,7 +94,7 @@ public class SecurityConfiguration {
 
         config.setAllowCredentials(true);
         config.setAllowedOrigins(List.of("http://ec2-52-78-15-107.ap-northeast-2.compute.amazonaws.com","http://localhost:3000"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS","HEAD"));
+        config.setAllowedMethods(List.of("*"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
 
