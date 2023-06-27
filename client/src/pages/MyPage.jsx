0fc0ed211ac9fb80c65fc2d89a4d7e4570f13447
1abme <124783124+1abme@users.noreply.github.com>
2023-06-27 11:59:21 +0900

    Update MyPage.jsx

--- client/src/pages/MyPage.jsx
+++ client/src/pages/MyPage.jsx
@@ -332,13 +332,6 @@ function MyPage() {
                 })
             )}
           </MyPageInfoPanel>
-          <Pagination
-            total={(userData[`${panel.id}List`] || []).length}
-            limit={5}
-            page={1}
-            setPage={(newPage) => handlePanelPageChange(panel.id, newPage)}
-            setLimit={(newLimit) => handlePanelLimitChange(panel.id, newLimit)}
-          />
         </MyPageInfoPanelContainer>
       ))}
     </MyPageContainer>
